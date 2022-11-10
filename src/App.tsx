a26cb2a923704966c5daa5d307c46014355a0b53
phamvant <pham.t.286.01@gmail.com>
2022-11-11 00:18:16 +0700

    --amend

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import React, { useEffect, useRef } from "react";
 import PawButton from "./components/PawButton";
 import Heart from "./pages/Heart";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/heart" element={<Heart />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
